86fb143a040f5b4f0f79994ef39ed209c7a33756
Antonio Fdez <antoniofernandezvara@gmail.com>
2020-05-03 00:24:02 +0200

    first commit

---
+++ tree.go
@@ -0,0 +1,128 @@
+package tree
+
+import (
+	"strings"
+)
+
+const (
+	// DefaultSeparator is the string that denotes each part of the path
+	DefaultSeparator = "/"
+)
+
+// MapTree manages data on a map to help returning data hierarchy
+type MapTree struct {
+	root      *Leaf
+	separator string
+}
+
+// Leaf is the unit that contains data and sub leaves
+type Leaf struct {
+	Leafs    map[string]Leaf `json:"items,omitempty"`
+	Metadata interface{}     `json:"meta,omitempty"`
+	Data     interface{}     `json:"data,omitempty"`
+}
+
+// New returns an empty MapTree
+func New() *MapTree {
+	return &MapTree{
+		root: &Leaf{
+			Leafs: make(map[string]Leaf),
+		},
+		separator: DefaultSeparator,
+	}
+}
+
+// Separator allows to change the separator with a custom one
+func (m *MapTree) Separator(separator string) {
+
+	m.separator = separator
+
+}
+
+// Tree returns the root leaf that contains the tree
+func (m *MapTree) Tree() *Leaf {
+
+	return m.root
+}
+
+// Set adds a new leaf on the tree
+func (m *MapTree) Set(key string, metadata, data interface{}) bool {
+
+	return m.recursiveInsert(m.root, key, metadata, data, 0)
+
+}
+
+// Delete the data from a leaf on the tree
+func (m *MapTree) Delete(key string) bool {
+
+	return m.recursiveRemoval(m.root, key, 0)
+
+}
+
+func (m *MapTree) recursiveInsert(l *Leaf, key string, metadata, data interface{}, depth int) bool {
+
+	var (
+		parts    []string
+		thisLeaf Leaf
+		ok       bool
+		isNew    bool
+	)
+
+	parts = strings.Split(key, m.separator)
+
+	_, ok = l.Leafs[parts[depth]]
+	if !ok {
+		// create leaf
+		l.Leafs[parts[depth]] = Leaf{
+			Leafs: make(map[string]Leaf),
+		}
+		isNew = true
+	}
+
+	thisLeaf = l.Leafs[parts[depth]]
+
+	// is the final leaf?
+	if depth == len(parts)-1 {
+		thisLeaf.Metadata = metadata
+		thisLeaf.Data = data
+		l.Leafs[parts[depth]] = thisLeaf
+	} else {
+		m.recursiveInsert(&thisLeaf, key, metadata, data, depth+1)
+	}
+
+	return isNew
+
+}
+
+func (m *MapTree) recursiveRemoval(l *Leaf, key string, depth int) bool {
+
+	var (
+		parts    []string
+		thisLeaf Leaf
+		ok       bool
+	)
+
+	parts = strings.Split(key, m.separator)
+
+	// we try to remove now
+	if depth == len(parts)-1 {
+		// full subtree?
+		_, ok = l.Leafs[parts[depth]]
+
+		if ok {
+			delete(l.Leafs, parts[depth])
+		}
+
+		return ok // return if deleted
+
+	}
+
+	// subkey exists? then walk inside
+	thisLeaf, ok = l.Leafs[parts[depth]]
+	if ok {
+		return m.recursiveRemoval(&thisLeaf, key, depth+1)
+	}
+
+	return ok
+
+}
---
+++ tree_test.go
@@ -0,0 +1,38 @@
+package tree
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewTree(t *testing.T) {
+
+	assert := assert.New(t)
+
+	assert.Equal("a", "a", "Must be equal")
+
+	aTree := New()
+
+	aTree.Set("a", "metaa", "dataa")
+	aTree.Set("a/a", "metaaa", "dataaa")
+
+	assert.Len(aTree.Tree().Leafs, 1, "Must have 1 leaf")
+	assert.True(aTree.Set("this/is/a/long/path", "metadata", "data"), "This must be a new record.")
+	assert.False(aTree.Set("this/is/a/long/path", "metadata'", "data'"), "This must be an update.")
+	assert.Len(aTree.Tree().Leafs, 2, "Must have 2 sub leaves")
+
+	assert.False(aTree.Delete("non-existent-entry"), "This must be false, entry does not exists")
+	assert.False(aTree.Delete("this/is/a/non-existent/path"), "This must be false, entry does not exists")
+	assert.True(aTree.Delete("this/is/a/long/path"), "This must be true, entry does exists")
+
+	aTree.Separator(".")
+	assert.Equal(aTree.separator, ".")
+	assert.True(aTree.Delete("this.is"), "Must be true")
+	assert.Len(aTree.Tree().Leafs, 2, "Must have 2 sub leaves")
+	assert.True(aTree.Delete("this"), "Must be true")
+	assert.Len(aTree.Tree().Leafs, 1, "Must have 1 leaf")
+	assert.True(aTree.Delete("a"), "Must be true, it deletes the rest of the tree")
+	assert.Len(aTree.Tree().Leafs, 0, "Must be empty")
+
+}
